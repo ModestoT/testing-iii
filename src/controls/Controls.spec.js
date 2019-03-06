cc66883270c00d43f45226c959d435dfec34187d
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-06 16:00:44 -0600

    added test for checking if the buttons when clicked will run a function

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,30 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+
+    it('Should fire a function when the button Lock is clicked when Closed prop is true', () => {
+        const onClick = jest.fn();
+        const { getByText } = render(<Controls toggleLocked={onClick} closed={true}/>);
+
+        const lockButton = getByText(/lock/i);
+        fireEvent.click(lockButton);
+
+        expect(onClick).toHaveBeenCalled();
+    });
+
+    it('Should fire a function when the close gate button is clicked', () => {
+        const onClick = jest.fn();
+        const { getByText } = render(<Controls toggleClosed={onClick}/>);
+
+        const closeButton = getByText(/close gate/i);
+        fireEvent.click(closeButton);
+
+        expect(onClick).toHaveBeenCalled();
+    });
+})
