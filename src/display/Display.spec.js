eb6d4bbb8c8027cb3b40d7f85b6fa7e097b173b7
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-06 15:18:14 -0600

    Wrote out test for ensuring that unlocked and open are being displayed by default, ensuring closed is displayed if the prop of closed is true and Locked for it's prop as well.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,42 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+import Display from './Display';
+
+
+describe('<Display />', () => {
+    it('should display as unlocked and open', () => {
+        const { getByText } = render(<Display />);
+
+        const unlocked = getByText(/unlocked/i);
+        expect(unlocked).toBeTruthy();
+
+        const open = getByText(/open/i);
+        expect(open).toBeTruthy();
+    });
+
+    it('should display "Closed" if closed prop is true', () => {
+        const { getByText } = render(<Display closed={true} />);
+
+        expect(getByText(/closed/i)).toBeTruthy();
+    });
+
+    it('should display "Locked" if locked prop is true', () => {
+        const { queryByText } = render(<Display locked={true}/>);
+
+        expect(queryByText(/unlocked/i)).toBeFalsy();
+    });
+
+    it('should have the class name red-led if it is locked or closed', () => {
+        const { getByText } = render(<Display locked={true} closed={true} />);
+        
+        const closed = getByText(/closed/i);
+        expect(closed).toHaveClass('red-led');
+
+        const locked = getByText(/locked/i);
+        expect(locked).toHaveClass('red-led');
+    });
+});
