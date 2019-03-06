2a4111c9591e01c0ec4d78417f1f3b66986699e1
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-06 16:48:33 -0600

    Added test to check if the buttons were disabled if the gate is locked or if the gate is opened

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -27,4 +27,31 @@ describe('<Controls />', () => {
 
         expect(onClick).toHaveBeenCalled();
     });
+
+    it('Should change the text of the button close gate button when clicked on to open gate', () => {
+        const onClick = jest.fn();
+        const { getByText, queryByText,rerender } = render(<Controls toggleClosed={onClick}/>);
+
+        const closeButton = getByText(/close gate/i);
+        expect(queryByText(/open gate/i)).toBeFalsy();
+
+        fireEvent.click(closeButton);
+        rerender(<Controls closed={true}/>);
+        
+        expect(queryByText(/open gate/i)).toBeTruthy();        
+    });
+
+    it('lock button should be disabled if the gate is open', () => {
+        const { getByText } = render(<Controls />);
+
+        const lockButton = getByText(/lock gate/i);
+
+        expect(lockButton).toHaveAttribute('disabled');
+    });
+
+    it('open gate should be disabled if the gate is locked', () => {
+        const { getByText} = render(<Controls closed={true} locked={true}/>);
+
+        expect(getByText(/open gate/i)).toHaveAttribute('disabled');
+    })
 })
